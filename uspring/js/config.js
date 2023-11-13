6e117c00b257ce9b037239723bb3e502da44a7ed
SergioNicolasGomezs <sergionicolasgomez2021@gmail.com>
2023-11-12 21:13:29 -0300

    se actualizo el js

--- uspring/js/config.js
+++ uspring/js/config.js
@@ -7,8 +7,18 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     var password = document.getElementById("contra").value;
     var comment = document.getElementById("coment").value;
 
+    if (user === '' || password === '') {
+        alert('Por favor, completa todos los campos');
+      } else {
+        // Si los campos están completos, puedes hacer algo aquí, como enviar el formulario o realizar otras acciones.
+        // Por ahora, solo mostramos un mensaje de éxito.
+        alert('Formulario enviado con éxito');
+      }
+
     // Puedes hacer lo que quieras con las variables, por ejemplo, mostrarlas en la consola
     console.log("usuario: " + user);
     console.log("contra: " + password);
     console.log("coment: " + comment);
 });
+
+
